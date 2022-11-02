d43dfaa21eb89e830b07ad3b3b86e7aa1311822d
Valor Nash <vnash@curtisswright.com>
2022-11-02 10:32:29 -0700

    Updated Form1.cs

--- NoteTaking/Form1.cs
+++ NoteTaking/Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,6 +11,8 @@ namespace NoteTaking
 {
 	public partial class Form1 : Form
 	{
+		DataTable table;
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -22,7 +25,25 @@ namespace NoteTaking
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			table = new DataTable();
+			table.Columns.Add("Title", typeof(String));
+			table.Columns.Add("Messages", typeof(String));
 
+			dataGridView1.DataSource = table;
+		}
+
+		private void bttNew_Click(object sender, EventArgs e)
+		{
+			TxtTitle.Clear();
+			txtMessage.Clear();
+		}
+
+		private void bttSave_Click(object sender, EventArgs e)
+		{
+			table.Rows.Add(TxtTitle.Text, txtMessage.Text);
+
+			TxtTitle.Clear();
+			txtMessage.Clear();
 		}
 	}
 }
